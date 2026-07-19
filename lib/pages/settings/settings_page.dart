20cfc433aa38d0ed91ec1c3309bc64c2cfa57d8d
lll152 <1528724436@qq.com>
2026-07-19 14:50:22 +0800

    fix: 移除未使用的 yaml 导入(analyze CI 报 warning)

--- lib/pages/settings/settings_page.dart
+++ lib/pages/settings/settings_page.dart
@@ -19,7 +19,6 @@ import 'package:venera/utils/data.dart';
 import 'package:venera/utils/data_sync.dart';
 import 'package:venera/utils/io.dart';
 import 'package:venera/utils/translations.dart';
-import 'package:yaml/yaml.dart';
 
 part 'reader.dart';
 part 'explore_settings.dart';
